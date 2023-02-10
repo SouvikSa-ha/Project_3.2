7e79260ec03f586475b0de354ba3e8bf6bba9658
Souvik Saha <souviksaha761@gmail.com>
2023-02-10 13:47:14 +0600

    done

--- DevCom/DevCom/App_Start/BundleConfig.cs
+++ DevCom/DevCom/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace DevCom
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/Site.css"));
         }
     }
 }
