d3578f07ba0ca704e90fdc8326c51bc3c08db05d
Souvik Saha <souviksaha761@gmail.com>
2023-02-10 14:17:46 +0600

    com

--- DevCom/DevCom/App_Start/BundleConfig.cs
+++ DevCom/DevCom/App_Start/BundleConfig.cs
@@ -19,8 +19,10 @@ namespace DevCom
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/bootstrap.bundle.min.js",
+                      "~/Scripts/bootstrap.esp.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
