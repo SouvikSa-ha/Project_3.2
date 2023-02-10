0abfd82f00cca7040f359e5bb21a42ef257a7cfe
Souvik Saha <souviksaha761@gmail.com>
2023-02-11 03:01:46 +0600

    noteindex

--- DevCom/DevCom/App_Start/RouteConfig.cs
+++ DevCom/DevCom/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DevCom
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Notepad", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ DevCom/DevCom/Scripts/notepadIndex.js
@@ -0,0 +1,11 @@
+﻿
+var buttons = document.querySelectorAll(".notes");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        var item = document.querySelector('.active');
+        (item) ? item.classList.remove('active') : '';
+        this.classList.add('active');
+    })
+}
+console.log("hello");
